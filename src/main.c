b49ff9ef6a03adc2e17f272cc4bb5c973a6eeda8
pryort <143113190+pryort@users.noreply.github.com>
2026-03-11 13:10:44 -0400

    initial joystick

--- src/main.c
+++ src/main.c
@@ -4,6 +4,24 @@
 #include <string.h>
 #include "pico/stdlib.h"
 
-int main() {
+void init_adc();
 
+void init_adc() {
+    gpio_init(45);
+    adc_init();
+    adc_gpio_init(45);
+    adc_select_input(5);
+}
+
+int main() {
+    stdio_init_all();
+
+    init_adc();
+
+    for(;;) {
+        printf("ADC Result: %d     \r", read_adc());
+
+        fflush(stdout);
+        sleep_ms(250);
+    }
 }
